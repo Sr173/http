4e35d05be2adc84e61e35115c6f05bd080adc602
admin <admin@DESKTOP-N4D9QPR>
2020-12-22 02:03:53 +0800

    init

---
+++ api_test.go
@@ -0,0 +1,145 @@
+package http_test
+
+import (
+	"crypto/subtle"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	lua_http "github.com/vadv/gopher-lua-libs/http"
+	inspect "github.com/vadv/gopher-lua-libs/inspect"
+	plugin "github.com/vadv/gopher-lua-libs/plugin"
+	lua_time "github.com/vadv/gopher-lua-libs/time"
+	lua "github.com/yuin/gopher-lua"
+)
+
+func basicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorised.\n"))
+			return
+		}
+		handler(w, r)
+	}
+}
+
+func httpCheckHeaders(w http.ResponseWriter, r *http.Request) {
+	if header := r.Header.Get(`simple_header`); header != `check_header` {
+		w.WriteHeader(412)
+		return
+	}
+	w.Write([]byte(`OK`))
+}
+
+func httpCheckUserAgent(w http.ResponseWriter, r *http.Request) {
+	if ua := r.UserAgent(); ua != `check_ua` {
+		w.WriteHeader(412)
+		return
+	}
+	w.Write([]byte(`OK`))
+}
+
+func httpRouterGet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`OK`))
+}
+
+func httpRouterGetTimeout(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(2 * time.Second)
+	w.Write([]byte(`OK`))
+}
+
+func runHttp(addr string) {
+	err := http.ListenAndServe(addr, nil) // задаем слушать порт
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func runHttps(addr string) {
+	err := http.ListenAndServeTLS(addr, "./test/server.crt", "./test/server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func request(url string) error {
+	client := &http.Client{}
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if strings.HasPrefix(url, string(body)) {
+		return fmt.Errorf("bad url, get: %s except: %s\n", body, url)
+	}
+	return nil
+}
+
+func manyRequest(addr string) {
+	time.Sleep(5 * time.Second)
+	count := 0
+	for {
+		if count > 10 {
+			break
+		}
+		url := fmt.Sprintf("%s/%s?d=%d", addr, "url", count)
+		go func(url string) {
+			if err := request(url); err != nil {
+				panic(err)
+			}
+		}(url)
+		count++
+	}
+}
+
+func TestApi(t *testing.T) {
+
+	http.HandleFunc("/get", httpRouterGet)
+	http.HandleFunc("/getBasicAuth", basicAuth(httpRouterGet, "admin", "123456", "Please enter your username and password for this site"))
+	http.HandleFunc("/timeout", httpRouterGetTimeout)
+	http.HandleFunc("/checkHeader", httpCheckHeaders)
+	http.HandleFunc("/checkUserAgent", httpCheckUserAgent)
+
+	go runHttp(":1111")
+	go runHttps(":1112")
+	time.Sleep(time.Second)
+
+	state := lua.NewState()
+	lua_http.Preload(state)
+	lua_time.Preload(state)
+	inspect.Preload(state)
+	plugin.Preload(state)
+
+	if err := state.DoFile("./test/test_client.lua"); err != nil {
+		t.Fatalf("execute test: %s\n", err.Error())
+	}
+
+	go manyRequest("http://127.0.0.1:1113")
+	if err := state.DoFile("./test/test_server_accept.lua"); err != nil {
+		t.Fatalf("execute test: %s\n", err.Error())
+	}
+
+	go manyRequest("http://127.0.0.1:2113")
+	if err := state.DoFile("./test/test_server_handle.lua"); err != nil {
+		t.Fatalf("execute test: %s\n", err.Error())
+	}
+
+	if err := state.DoFile("./test/test_server_accept_stop.lua"); err != nil {
+		t.Fatalf("execute test: %s\n", err.Error())
+	}
+
+	if err := state.DoFile("./test/test_serve_static.lua"); err != nil {
+		t.Fatalf("execute test: %s\n", err.Error())
+	}
+}
---
+++ client/request.go
@@ -0,0 +1,170 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	lua "github.com/yuin/gopher-lua"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"time"
+	"unsafe"
+)
+
+type luaRequest struct {
+	*http.Request
+}
+
+func checkRequest(L *lua.LState, n int) *luaRequest {
+	ud := L.CheckUserData(n)
+	if v, ok := ud.Value.(*luaRequest); ok {
+		return v
+	}
+	L.ArgError(1, "http request excepted")
+	return nil
+}
+
+// http.request(verb, url, body) returns user-data, error
+func NewRequest(L *lua.LState) int {
+	verb := L.CheckString(1)
+	url := L.CheckString(2)
+	buffer := &bytes.Buffer{}
+	if L.GetTop() > 2 {
+		buffer.WriteString(L.CheckString(3))
+	}
+	httpReq, err := http.NewRequest(verb, url, buffer)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+
+	req := &luaRequest{Request: httpReq}
+	req.Request.Header.Set(`User-Agent`, DefaultUserAgent)
+	ud := L.NewUserData()
+	ud.Value = req
+	L.SetMetatable(ud, L.GetTypeMetatable("http_request_ud"))
+	L.Push(ud)
+	return 1
+}
+
+// request:set_basic_auth(username, password)
+func SetBasicAuth(L *lua.LState) int {
+	req := checkRequest(L, 1)
+	user, passwd := L.CheckAny(2).String(), L.CheckAny(3).String()
+	req.SetBasicAuth(user, passwd)
+	return 0
+}
+
+// request:header_set(key, value)
+func HeaderSet(L *lua.LState) int {
+	req := checkRequest(L, 1)
+	key, value := L.CheckAny(2).String(), L.CheckAny(3).String()
+	req.Header.Set(key, value)
+	return 0
+}
+
+// DoRequest lua http_client_ud:do_request()
+// http_client_ud:do_request(http_request_ud) returns (response, error)
+//    response: {
+//      code = http_code (200, 201, ..., 500, ...),
+//      body = string
+//      headers = table
+//    }
+func DoRequest(L *lua.LState) int {
+	client := checkClient(L)
+	req := checkRequest(L, 2)
+
+	response, err := client.DoRequest(req.Request)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	defer response.Body.Close()
+	headers := L.NewTable()
+
+	for k, v := range response.Header {
+		if len(v) > 0 {
+			headers.RawSetString(k, lua.LString(v[0]))
+		}
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	result := L.NewTable()
+	L.SetField(result, `code`, lua.LNumber(response.StatusCode))
+	L.SetField(result, `body`, lua.LString(string(data)))
+	L.SetField(result, `headers`, headers)
+	L.Push(result)
+	return 1
+}
+
+
+func GetUnexportedField(field reflect.Value) interface{} {
+	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
+}
+
+func GetCookies(L *lua.LState) int {
+	client := checkClient(L)
+
+	o := reflect.ValueOf(client.Jar)
+
+	entries := reflect.Indirect(o).FieldByName("entries")
+	i := GetUnexportedField(entries)
+	result, _ := json.Marshal(i)
+	L.Push(lua.LString(result))
+
+	return 1
+}
+
+func GetCookie(L *lua.LState) int {
+	client := checkClient(L)
+	req := L.CheckString(2)
+
+	url,_ := url.Parse(req)
+	cookies := client.Client.Jar.Cookies(url)
+
+	result := L.NewTable()
+
+	if cookies == nil {
+		L.Push(lua.LNil)
+		return 1
+	}
+
+	for _,v := range(cookies){
+		L.SetField(result, v.Name, lua.LString(v.Value))
+	}
+
+	L.Push(result)
+	return 1
+}
+
+func SetHeader(L *lua.LState) int {
+	client := checkClient(L)
+	key := L.CheckString(2)
+	value := L.CheckString(3)
+
+	client.headers[key] = value
+
+	return 0
+}
+
+func SetCookie(L *lua.LState) int {
+	client := checkClient(L)
+	url_s := L.CheckString(2)
+	key := L.CheckString(3)
+	value := L.CheckString(4)
+
+	url,_ := url.Parse(url_s)
+	expiration := time.Now().Add(24 * time.Hour)
+	cookie := http.Cookie{Name: key, Value: value, Expires: expiration}
+	client.Client.Jar.SetCookies(url, []*http.Cookie{&cookie})
+
+	return 0
+}
